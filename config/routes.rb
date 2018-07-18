24f46aa2f99d0f05815d2ff769b7dd9a5ce365b3
Rob Kaufman <rob@notch8.com>
2018-07-18 16:53:20 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :contacts, only: [:new, :create]
-  resources :visitors, only: [:new, :create]
-  root to: 'visitors#new'
+  scope ENV['RAILS_RELATIVE_URL_ROOT'] || '/' do
+    resources :contacts, only: [:new, :create]
+    resources :visitors, only: [:new, :create]
+    root to: 'visitors#new'
+  end
 end
